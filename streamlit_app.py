06423a12ebdc845ac0fa6279b276565a0e0d51fc
Jsnodal <139485865+Jsnodal@users.noreply.github.com>
2024-11-14 13:17:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🎈 MachineLearning App')
 
 st.info('This is app builds a machine leaning model!')
 
-data = pd.read_csv("https://github.com/dataprofessor/palmer-penguins/commit/4dbd79d6ae9f5f97ec9682e79e8ce73c150aca1f")
+data = pd.read_csv("penguins_cleaned.csv")
 data
