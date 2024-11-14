6bf47c0cde9f44dd1c4339220f7435f187b197bc
Jsnodal <139485865+Jsnodal@users.noreply.github.com>
2024-11-14 13:26:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,16 @@ st.title('🎈 MachineLearning App')
 
 st.info('This is app builds a machine leaning model!')
 
-data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-data
+with set.expander('data'):
+  st.write('**Raw Data**')
+
+  data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  data
+
+st.write('**X**')
+X = data.drop('species',axis = 1)
+X
+
+st.write('**y**')
+y = data.species
+y
