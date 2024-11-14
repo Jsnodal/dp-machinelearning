c1d9db3de7f0780dbf4bbfa290429eceb19cc08b
Jsnodal <139485865+Jsnodal@users.noreply.github.com>
2024-11-14 13:14:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 MachineLearning App')
 
 st.info('This is app builds a machine leaning model!')
+
+data = pd.read_csv("https://github.com/dataprofessor/palmer-penguins/commit/4dbd79d6ae9f5f97ec9682e79e8ce73c150aca1f")
+data
